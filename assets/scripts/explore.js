e8e4355eee7d4ec8e566a947bf1a344929f24d37
greyluo <luo80480@gmail.com>
2022-10-28 17:12:27 -0700

    explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,9 +1,35 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-
+  const synth = window.speechSynthesis;
+  let img = document.querySelector('img');
+  img.src='assets/images/smiling.png';
+  const voiceSelect = document.querySelector('select');
+  synth.addEventListener('voiceschanged', () => {
+    const voices = synth.getVoices();
+    voices.forEach((voice) => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      voiceSelect.appendChild(option);
+    }
+    );
+  });
+  document.querySelector('button').addEventListener('click', () => {
+    let utter = new SpeechSynthesisUtterance(document.querySelector('textarea').value);
+    let selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    const voices = synth.getVoices();
+    utter.voice = voices.find((voice) => {
+      voice.name === selectedOption
+    });
+    synth.speak(utter);
+    img.src = 'assets/images/smiling-open.png';
+    utter.addEventListener('end', () => {
+      img.src = 'assets/images/smiling.png';
+    });
+  });
   
 }
